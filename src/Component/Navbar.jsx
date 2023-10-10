a8c60b90a91ad34e52889239a62df72202d2ef9f
Shirak <shirak_serop_6@hotmail.com>
2023-10-10 14:08:49 +0100

    Navbar and draggable

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+    return ( 
+        <nav className="navbar">
+            <section className="navbar_title">
+                <h3>Title</h3>
+            </section>
+            <section className="navbar_logo">
+                <h1>PlannYH</h1>
+            </section>
+            <section className="navbar_views">
+                <h3>Views</h3>
+            </section>
+        </nav>
+
+     );
+}
+
+export default Navbar ;
